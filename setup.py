4cf4832ce1e1a340fd0666f2c0cd05fd6582512d
erdewit <ewald.de.wit@gmail.com>
2018-09-18 23:16:09 +0200

    v0.8.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='heartwave',
-    version='0.8.5',
+    version='0.8.6',
     description='Heart rate measurement from webcam or video',
     long_description=long_description,
     packages=['heartwave'],
